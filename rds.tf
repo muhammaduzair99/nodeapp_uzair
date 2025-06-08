6728409d0b7bd8b93eaf9a4dac50c41c97d09f11
Muhammad Uzair <muhammad.uzair@contour-software.com>
2025-06-08 23:13:15 +0500

    progress

--- rds.tf
+++ rds.tf
@@ -1,11 +1,18 @@
+# Subnet group for both databases
 resource "aws_db_subnet_group" "default" {
   name       = "default-db-subnet-group"
   subnet_ids = data.aws_subnets.default.ids
+
+  tags = {
+    Name = "default-db-subnet-group"
+  }
 }
 
+# MySQL RDS Instance
 resource "aws_db_instance" "mysql" {
   identifier             = "mysql-db"
   engine                 = "mysql"
+  engine_version         = "8.0"
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
   username               = "muzair12"
@@ -15,4 +22,28 @@ resource "aws_db_instance" "mysql" {
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.default.name
+
+  tags = {
+    Name = "mysql-db"
+  }
+}
+
+# PostgreSQL RDS Instance
+resource "aws_db_instance" "postgresql" {
+  identifier             = "postgres-db"
+  engine                 = "postgres"
+  engine_version         = "17.4"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  username               = "muzair12"
+  password               = "StrongPwd123!"
+  db_name                = "mydb"
+  publicly_accessible    = false
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.default.name
+
+  tags = {
+    Name = "postgres-db"
+  }
 }
